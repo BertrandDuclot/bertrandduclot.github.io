3e89e2a590ba8fa9fe68db50b12d26ac690830fa
Bertrand D <b.duclot@gmail.com>
2023-12-04 14:52:00 +0100

    files

---
+++ script.js
@@ -0,0 +1,6 @@
+// Print email address on clicking email icon
+const email = document.querySelector(".fa-envelope");
+
+email.addEventListener("click", () => {
+  alert("Adresse email : b.duclot@gmail.com");
+});
